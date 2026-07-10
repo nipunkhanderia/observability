75b0da4a80dcce6948135bd9ea0c7b635c4c617c
nipunkhanderia <nipunkhanderia@gmail.com>
2026-07-10 15:44:38 +0000

    metadata added

--- obs.py
+++ obs.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+langfuse = Langfuse()
 
 @observe
-def llm_response():
+def llm_response(input):
+    langfuse.update_current_generation(model = "llama3.2", metadata = {"temperature":0.7,"max_tokens":100})
     return ("Nipun rocks")
 
-llm_response()
+llm_response("Who is Nipun and what does he do?")
 
 
